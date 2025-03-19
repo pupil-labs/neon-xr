388fc5284e1871564591cf88807a13f67d28dfc5
Juraj Vincur <juraj.vincur@gmail.com>
2025-03-19 18:57:17 +0100

    Eyelid added to eye state vis

--- com.pupil-labs.neon-xr.core/Runtime/Scritps/EyeStateVisualizer.cs
+++ com.pupil-labs.neon-xr.core/Runtime/Scritps/EyeStateVisualizer.cs
@@ -14,17 +14,29 @@ namespace PupilLabs
         [SerializeField]
         protected Transform leftPupil;
         [SerializeField]
+        protected Transform leftEyelidTop;
+        [SerializeField]
+        protected Transform leftEyelidBottom;
+        [SerializeField]
         protected Transform rightEye;
         [SerializeField]
         protected Transform rightPupil;
+        [SerializeField]
+        protected Transform rightEyelidTop;
+        [SerializeField]
+        protected Transform rightEyelidBottom;
 
         protected bool eyeStateAvailable = false;
         protected EyeState eyeState;
+        protected bool eyelidAvailable = false;
+        protected Eyelid eyelid;
 
         public void OnGazeDataReady(GazeDataProvider gazeDataProvider)
         {
             eyeStateAvailable = gazeDataProvider.EyeStateAvailable;
             eyeState = gazeDataProvider.EyeState;
+            eyelidAvailable = gazeDataProvider.EyelidAvailable;
+            eyelid = gazeDataProvider.Eyelid;
         }
 
         protected virtual void LateUpdate()
@@ -50,6 +62,20 @@ namespace PupilLabs
             tmp = rightPupil.localScale;
             tmp.x = tmp.z = eyeState.pupilDiameterRight;
             rightPupil.localScale = tmp;
+
+            if (eyelidAvailable == false)
+            {
+                leftEyelidTop.gameObject.SetActive(false);
+                leftEyelidBottom.gameObject.SetActive(false);
+                rightEyelidTop.gameObject.SetActive(false);
+                rightEyelidBottom.gameObject.SetActive(false);
+                return;
+            }
+
+            leftEyelidTop.localEulerAngles = new Vector3(-eyelid.eyelidAngleTopLeft, 0, 0);
+            leftEyelidBottom.localEulerAngles = new Vector3(-eyelid.eyelidAngleBottomLeft, 0, 0);
+            rightEyelidTop.localEulerAngles = new Vector3(-eyelid.eyelidAngleTopRight, 0, 0);
+            rightEyelidBottom.localEulerAngles = new Vector3(-eyelid.eyelidAngleBottomRight, 0, 0);
         }
 
         protected virtual void OnEnable()
